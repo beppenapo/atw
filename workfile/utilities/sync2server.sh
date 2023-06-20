543f6ff0c19c26c2b46df63218450ff810836a1b
Giuseppe Naponiello <beppenapo@arc-team.com>
2023-06-20 11:57:07 +0200

    utilities

--- workfile/utilities/sync2server.sh
+++ workfile/utilities/sync2server.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-rsync -varzhP --exclude '/var/www/html/atw/api/config/conn.class.php' --delete /var/www/html/atw/ beppe@91.121.82.80:/var/www/arc-team.com/atw/
+rsync -varzhP --exclude '/var/www/html/atw/api/config/db.ini' --delete /var/www/html/atw/ beppe@91.121.82.80:/var/www/atw/
